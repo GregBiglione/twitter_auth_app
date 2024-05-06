f09df4bd446debcb1d03f2ad523c796ced55c32f
GregBiglione <gregory.biglione81@gmail.com>
2024-05-06 03:11:52 +0200

    Added images, Constant & ImageManager

---
+++ lib/app/constant.dart
@@ -0,0 +1,1 @@
+const IMAGE_PATH = "assets/images";
---
+++ lib/presentation/resource/image_manager.dart
@@ -0,0 +1,6 @@
+import '../../app/constant.dart';
+
+class ImageManager {
+  static const String twitterLogo = "$IMAGE_PATH/twitter.png";
+  static const String xLogo = "$IMAGE_PATH/x-logo.png";
+}
