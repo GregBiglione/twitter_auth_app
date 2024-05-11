1e07009e55b401fd71b1d8db2b781f34bb19934f
GregBiglione <gregory.biglione81@gmail.com>
2024-05-11 04:19:13 +0200

    Added TwitterButtonWidget

--- lib/presentation/screen/login/widget/login_content.dart
+++ lib/presentation/screen/login/widget/login_content.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:twitter_auth_app/presentation/resource/color_manager.dart';
-import 'package:twitter_auth_app/presentation/resource/font_manager.dart';
 import 'package:twitter_auth_app/presentation/resource/string_manager.dart';
-import 'package:twitter_auth_app/presentation/resource/style_manager.dart';
 import 'package:twitter_auth_app/presentation/resource/value_manager.dart';
 import 'package:twitter_auth_app/presentation/widget/padding_widget.dart';
 import 'package:twitter_auth_app/presentation/widget/title_widget.dart';
+import 'package:twitter_auth_app/presentation/widget/twitter_button_widget.dart';
 
 import '../../../../app/constant.dart';
 
@@ -38,66 +35,13 @@ class _LoginContentState extends State<LoginContent> {
         const SizedBox(
           height: ValueManager.v20,
         ),
-        _twitterButtonWidget()
+        TwitterButtonWidget(
+          onPressed: () {
+            logger.i("Twitter button clicked");
+          },
+        ),
       ],
     ),
-  );
-
-  //----------------------------------------------------------------------------
-  // Twitter button
-  //----------------------------------------------------------------------------
-
-  Widget _twitterButtonWidget() => GestureDetector(
-    child: Padding(
-      padding: const EdgeInsets.only(
-        left: ValueManager.v16,
-        right: ValueManager.v16,
-      ),
-      child: Container(
-        width: ValueManager.infinity,
-        height: ValueManager.v50,
-        decoration: BoxDecoration(
-          color: ColorManager.primary,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(
-              ValueManager.v30,
-            ),
-          ),
-        ),
-        child: Row(
-          children: [
-            const SizedBox(
-              width: ValueManager.v20,
-            ),
-            // Logo --------------------------------------------------------------
-            FaIcon(
-              FontAwesomeIcons.twitter,
-              color: ColorManager.white,
-              size: ValueManager.v30,
-            ),
-            // Title -------------------------------------------------------------
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  right: ValueManager.v50,
-                ),
-                child: Text(
-                  StringManager.twitterButtonTitle,
-                  style: getBoldStyle(
-                    fontSize: FontSize.s18,
-                    color: ColorManager.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
-      )
-    ),
-    onTap: () {
-      logger.i("Twitter button clicked");
-    },
   );
 }
 
