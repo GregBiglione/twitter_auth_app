01f4341b0e7bcd96106770a40dd95229dba1420b
GregBiglione <gregory.biglione81@gmail.com>
2024-05-13 12:47:09 +0200

    Linked project to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_auth_app/firebase_options.dart';
 import 'package:twitter_auth_app/presentation/resource/route_manager.dart';
 import 'package:twitter_auth_app/presentation/resource/theme_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
