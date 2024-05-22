664f5b512bc9602c6aaf2154b8f83c72aef4ba46
GregBiglione <gregory.biglione81@gmail.com>
2024-05-22 16:23:34 +0200

    Added FirebaseService

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:twitter_auth_app/firebase_options.dart';
 import 'package:twitter_auth_app/presentation/resource/route_manager.dart';
 import 'package:twitter_auth_app/presentation/resource/theme_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
