dbc11a9fc8517ddb7276ea5fe2dcd411aac59944
GregBiglione <gregory.biglione81@gmail.com>
2024-05-29 16:22:14 +0200

    Added AppBar in HomeScreen

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_auth_app/presentation/resource/color_manager.dart';
+import 'package:twitter_auth_app/presentation/resource/font_manager.dart';
+import 'package:twitter_auth_app/presentation/resource/string_manager.dart';
+import 'package:twitter_auth_app/presentation/resource/style_manager.dart';
 import 'package:twitter_auth_app/presentation/screen/home/widget/home_content.dart';
+
+import '../../../app/constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +14,28 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.black,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: ColorManager.primary,
+        title: Text(
+          StringManager.homeTitle,
+          style: getBoldStyle(
+            fontSize: FontSize.s16,
+            color: ColorManager.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logger.i("Log out button clicked");
+            },
+            icon: Icon(
+              Icons.logout,
+              color: ColorManager.white,
+            ),
+          ),
+        ],
+      ),
       body: const HomeContent(),
     );
   }
