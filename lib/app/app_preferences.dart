eb80aeb58b7e8ab554bcecbdce1582ddd2384528
GregBiglione <gregory.biglione81@gmail.com>
2024-05-29 19:29:16 +0200

    Added AppPreferences

---
+++ lib/app/app_preferences.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:twitter_auth_app/app/constant.dart';
+
+class AppPreferences {
+  final SharedPreferences _sharedPreferences;
+
+  AppPreferences(this._sharedPreferences);
+
+  // ---------------------------------------------------------------------------
+  // Set preferences for user logged
+  // ---------------------------------------------------------------------------
+
+  Future<void> setUserLogged() async {
+    _sharedPreferences.setBool(IS_USER_LOGGED, true);
+  }
+
+  // ---------------------------------------------------------------------------
+  // Get preferences for user logged
+  // ---------------------------------------------------------------------------
+
+  Future<bool> isUserLogged() async {
+    return _sharedPreferences.getBool(IS_USER_LOGGED) ?? false;
+  }
+}
